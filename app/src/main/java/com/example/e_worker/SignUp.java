6d98910330a8fe2574966c7e9b1248aec03226e5
TahaRizwan <tahabhaiyag@gmail.com>
2021-02-07 21:37:24 +0500

    Validation  part 1

--- app/src/main/java/com/example/e_worker/SignUp.java
+++ app/src/main/java/com/example/e_worker/SignUp.java
@@ -17,9 +17,10 @@ public class SignUp extends AppCompatActivity {
     FirebaseDatabase rootNode;
     DatabaseReference reference;
 
-    TextInputLayout regName,regUsername,regEmail,regPhoneNo,regPassword;
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
     Button regBtn;
     Button callSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,35 +33,136 @@ public class SignUp extends AppCompatActivity {
         regPhoneNo = findViewById(R.id.reg_phoneNo);
         regPassword = findViewById(R.id.reg_password);
         regBtn = findViewById(R.id.reg_btn);
-        callSignIn =  findViewById(R.id.signin_screen);
-        regBtn.setOnClickListener(new View.OnClickListener(){
+        callSignIn = findViewById(R.id.signin_screen);
+
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
 
                 rootNode = FirebaseDatabase.getInstance("https://e-worker-fa0e4-default-rtdb.firebaseio.com/");
-                reference=rootNode.getReference("users");
+                reference = rootNode.getReference("users");
 
+                if(!validateName()|!validateUserName()|!validateEmail()|!validatePhoneNo()|!validatePassword()){
+
+                    return;
+
+                }
                 String name = regName.getEditText().getText().toString();
                 String username = regUsername.getEditText().getText().toString();
                 String email = regEmail.getEditText().getText().toString();
                 String phoneNo = regPhoneNo.getEditText().getText().toString();
                 String password = regPassword.getEditText().getText().toString();
 
-                UserHelperClass helperClass = new UserHelperClass(name,username,email,phoneNo,password);
+                UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
 
                 reference.child(username).setValue(helperClass);
 
             }
         });
-        callSignIn.setOnClickListener(new View.OnClickListener(){
+        callSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUp.this,Login.class);
+                Intent intent = new Intent(SignUp.this, Login.class);
                 startActivity(intent);
             }
         });
 
 
-
     }
+
+
+    private Boolean validateName() {
+        String val = regName.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            regName.setError("Field cannot be empty");
+            return false;
+        } else {
+            regName.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateUserName() {
+        String val = regUsername.getEditText().getText().toString();
+        String noWhiteSpace = "\\S+";
+        if (val.isEmpty()) {
+            regUsername.setError("Field cannot be empty");
+            return false;
+        }
+        else if(val.length()>=15){
+
+            regUsername.setError("Username too long");
+            return false;
+        }
+        else if(!val.matches(noWhiteSpace)){
+            regUsername.setError("No Whitespace allowed");
+            return false;
+        }
+        else {
+            regUsername.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail() {
+        String emailPattern = "([a-z]+)([_.a-z0-9]*)([a-z0-9]+)(@)([a-z]+)([.a-z]+)([a-z]+)";
+
+        String val = regEmail.getEditText().getText().toString();
+
+
+        if (val.isEmpty()) {
+            regEmail.setError("Field cannot be empty");
+            return false;
+        }
+        else if(!val.matches(emailPattern)){
+            regEmail.setError("Invalid email address");
+            return false;
+        }
+        else {
+            regEmail.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validatePhoneNo() {
+        String val = regPhoneNo.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            regPhoneNo.setError("Field cannot be empty");
+            return false;
+        } else {
+            regPhoneNo.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = regPassword.getEditText().getText().toString();
+        String passwordVal = "^"+
+                "(?=.*[0-9])"+
+                "(?=.*[a-z])"+
+                "(?=.*[A-Z])"+
+                "(?=.*[a-zA-Z])"+
+                "(?=.*[Q#$%^&+=])"+
+                "(?=\\s+$)"+
+                ".(4,)"+
+                "$";
+        if (val.isEmpty()) {
+            regPassword.setError("Field cannot be empty");
+            return false;
+        }
+        else if(val.matches(passwordVal)){
+            regPassword.setError("Password is too weak");
+            return false;
+        }
+        else {
+            regPassword.setError(null);
+            return true;
+
+        }
+    }
+
+
 }
